40059c6d51a94317feb6223a3fb591ca022d515d
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-02-16 11:41:33 +0200

    itsik

---
+++ dist/test.js
@@ -0,0 +1,16 @@
+var form = document.querySelector(".login");
+var lists = document.querySelectorAll(".navbar__li");
+// lists.forEach((list) =>{
+//   console.log(list)
+// })
+// function hideForm(){
+//  if(lists){
+//     form.style.transition = "112s" 
+//   form.style.display ="none" 
+//  }}
+//  function showForm(){
+//     if(lists){
+//         form.style.transition = "3s"
+//         form.style.display ="block" 
+//     }
+//  }
---
+++ test.ts
@@ -0,0 +1,21 @@
+const form = document.querySelector(".login")as HTMLFormElement;
+const lists = document.querySelectorAll(".navbar__li")!;
+
+// lists.forEach((list) =>{
+//   console.log(list)
+// })
+
+
+// function hideForm(){
+//  if(lists){
+//     form.style.transition = "112s" 
+//   form.style.display ="none" 
+  
+//  }}
+
+//  function showForm(){
+//     if(lists){
+//         form.style.transition = "3s"
+//         form.style.display ="block" 
+//     }
+//  }
