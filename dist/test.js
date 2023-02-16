51a8c7a59c7541bd380fc3abc2584e32fb76b00a
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-02-16 17:34:05 +0200

    itsik

--- dist/test.js
+++ dist/test.js
@@ -7,35 +7,35 @@ var showInfo = document.querySelector(".inFormer__showinfo");
 var showUS = document.querySelector(".inFormer__showUs");
 var showContact = document.querySelector(".inFormer__showContact");
 // events
-// info.addEventListener("mouseover", showTheInfo)
-// info.addEventListener("mouseout", hideTheInfo)
-// us.addEventListener("mouseover", showTheUs)
-// us.addEventListener("mouseout", hideTheUs)
-// contact.addEventListener("mouseover", showTheContact)
-// contact.addEventListener("mouseout", hideTheContact)
-// // info
-// function showTheInfo(){
-//   form.style.display = "none"  
-//   showInfo.style.display = "block"
-//   }
-// function hideTheInfo(){
-//   form.style.display = "block"  
-//   showInfo.style.display = "none"
-//   }
-// //  us
-// function showTheUs(){
-//   form.style.display = "none"  
-//   showUS.style.display = "block"
-// }
-// function hideTheUs(){
-//   form.style.display = "block"  
-//   showUS.style.display = "none"
-// }
-// function showTheContact(){
-//   form.style.display = "none"  
-//   showContact.style.display = "block"
-//   }
-// function hideTheContact(){
-//   form.style.display = "block"  
-//   showContact.style.display = "none"
-// }
+info.addEventListener("mouseover", showTheInfo);
+info.addEventListener("mouseout", hideTheInfo);
+us.addEventListener("mouseover", showTheUs);
+us.addEventListener("mouseout", hideTheUs);
+contact.addEventListener("mouseover", showTheContact);
+contact.addEventListener("mouseout", hideTheContact);
+// info
+function showTheInfo() {
+    form.style.display = "none";
+    showInfo.style.display = "block";
+}
+function hideTheInfo() {
+    form.style.display = "block";
+    showInfo.style.display = "none";
+}
+//  us
+function showTheUs() {
+    form.style.display = "none";
+    showUS.style.display = "block";
+}
+function hideTheUs() {
+    form.style.display = "block";
+    showUS.style.display = "none";
+}
+function showTheContact() {
+    form.style.display = "none";
+    showContact.style.display = "block";
+}
+function hideTheContact() {
+    form.style.display = "block";
+    showContact.style.display = "none";
+}
