017f8855e17ad8b52524242e156e2a01100b4721
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-02-16 12:48:40 +0200

    itsik

--- dist/test.js
+++ dist/test.js
@@ -1,16 +1,41 @@
 var form = document.querySelector(".login");
-var lists = document.querySelectorAll(".navbar__li");
-// lists.forEach((list) =>{
-//   console.log(list)
-// })
-// function hideForm(){
-//  if(lists){
-//     form.style.transition = "112s" 
-//   form.style.display ="none" 
-//  }}
-//  function showForm(){
-//     if(lists){
-//         form.style.transition = "3s"
-//         form.style.display ="block" 
-//     }
-//  }
+var info = document.querySelector("#li-Info");
+var us = document.querySelector("#li-Us");
+var contact = document.querySelector("#li-Contact");
+// the divs
+var showInfo = document.querySelector(".inFormer__showinfo");
+var showUS = document.querySelector(".inFormer__showUs");
+var showContact = document.querySelector(".inFormer__showContact");
+// events
+info.addEventListener("mouseover", showTheInfo);
+info.addEventListener("mouseout", hideTheInfo);
+us.addEventListener("mouseover", showTheUs);
+us.addEventListener("mouseout", hideTheUs);
+contact.addEventListener("mouseover", showTheContact);
+contact.addEventListener("mouseout", hideTheContact);
+// info
+function showTheInfo() {
+    form.style.display = "none";
+    showInfo.style.display = "block";
+}
+function hideTheInfo() {
+    form.style.display = "block";
+    showInfo.style.display = "none";
+}
+//  us
+function showTheUs() {
+    form.style.display = "none";
+    showUS.style.display = "block";
+}
+function hideTheUs() {
+    form.style.display = "block";
+    showUS.style.display = "none";
+}
+function showTheContact() {
+    form.style.display = "none";
+    showContact.style.display = "block";
+}
+function hideTheContact() {
+    form.style.display = "block";
+    showContact.style.display = "none";
+}
--- test.ts
+++ test.ts
@@ -1,21 +1,53 @@
 const form = document.querySelector(".login")as HTMLFormElement;
-const lists = document.querySelectorAll(".navbar__li")!;
-
-// lists.forEach((list) =>{
-//   console.log(list)
-// })
+const info = document.querySelector("#li-Info")as HTMLElement;
+const us = document.querySelector("#li-Us")as HTMLElement;
+const contact = document.querySelector("#li-Contact")as HTMLElement;
 
 
-// function hideForm(){
-//  if(lists){
-//     form.style.transition = "112s" 
-//   form.style.display ="none" 
-  
-//  }}
+// the divs
+const showInfo = document.querySelector(".inFormer__showinfo")as HTMLDivElement;
+const showUS = document.querySelector(".inFormer__showUs")as HTMLDivElement;
+const showContact = document.querySelector(".inFormer__showContact")as HTMLDivElement;
 
-//  function showForm(){
-//     if(lists){
-//         form.style.transition = "3s"
-//         form.style.display ="block" 
-//     }
-//  }
+    // events
+
+    info.addEventListener("mouseover", showTheInfo)
+    info.addEventListener("mouseout", hideTheInfo)
+    us.addEventListener("mouseover", showTheUs)
+    us.addEventListener("mouseout", hideTheUs)
+    contact.addEventListener("mouseover", showTheContact)
+    contact.addEventListener("mouseout", hideTheContact)
+    
+
+    // info
+    function showTheInfo(){
+      form.style.display = "none"  
+      showInfo.style.display = "block"
+    }
+
+    function hideTheInfo(){
+      form.style.display = "block"  
+      showInfo.style.display = "none"
+    }
+
+    //  us
+    function showTheUs(){
+      form.style.display = "none"  
+      showUS.style.display = "block"
+    }
+
+    function hideTheUs(){
+      form.style.display = "block"  
+      showUS.style.display = "none"
+    }
+    function showTheContact(){
+      form.style.display = "none"  
+      showContact.style.display = "block"
+    }
+
+    function hideTheContact(){
+      form.style.display = "block"  
+      showContact.style.display = "none"
+    }
+
+    
