46a556b0a4d3a721b497aba496b1abb90d241a2e
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-02-20 15:19:07 +0200

    i

--- view/dist/view.js
+++ view/dist/view.js
@@ -18,25 +18,25 @@ us.addEventListener("mouseout", hideTheUs);
 contact.addEventListener("mouseover", showTheContact);
 contact.addEventListener("mouseout", hideTheContact);
 //-----Calc. Page
-// const buttonInput = document.querySelectorAll(".calculator__buttons__button") as NodeListOf<HTMLInputElement>
-// const inputCalc = document.querySelector("#inputSum") as HTMLInputElement
-// const inputTitle = document.querySelector("#inputTitle") as HTMLInputElement
-// const buttonClear = document.querySelector("#clear") as HTMLButtonElement
-// const buttonAddToTotal = document.querySelector("#addToTotal") as HTMLButtonElement
-// window.onload = () => {
-//     inputCalc.value = ""
-//     inputTitle.value = ""
-// }
-// // events
-// buttonInput.forEach((button) => {
-//     button.addEventListener("click", () => {
-//         //display value of each button
-//         console.log("click")
-//         inputCalc.value += button.value
-//     })
-// })
-// //Clear the inputs
-// buttonClear.addEventListener("click", () => {
-//     inputCalc.value = ""
-//     inputTitle.value = ""
-// });
+var buttonInput = document.querySelectorAll(".calculator__buttons__button");
+var inputCalc = document.querySelector("#inputSum");
+var inputTitle = document.querySelector("#inputTitle");
+var buttonClear = document.querySelector("#clear");
+var buttonAddToTotal = document.querySelector("#addToTotal");
+window.onload = function () {
+    inputCalc.value = "";
+    inputTitle.value = "";
+};
+// events
+buttonInput.forEach(function (button) {
+    button.addEventListener("click", function () {
+        //display value of each button
+        console.log("click");
+        inputCalc.value += button.value;
+    });
+});
+//Clear the inputs
+buttonClear.addEventListener("click", function () {
+    inputCalc.value = "";
+    inputTitle.value = "";
+});
