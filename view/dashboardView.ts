992b5748971ca863e2c0d86fe0d6b90a74ecf928
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-02-23 10:26:17 +0200

    i

--- view/dashboardView.ts
+++ view/dashboardView.ts
@@ -1,29 +1,37 @@
 window.onload = spanWelcome
 let spanWel = document.querySelector(".container__h2Welcome__spanWelcome")as HTMLSpanElement;
+let sideBar = document.querySelectorAll("#sideBar")!;
 
-const showMonitor = document.querySelector(".showMonitor")as HTMLDivElement
-const showCalc = document.querySelector(".showCalculator")as HTMLDivElement
-
-let sideBar:any = document.querySelectorAll("#sideBar");
+ 
 if(sideBar){
-    sideBar.forEach((bars) =>{
-       bars.addEventListener("click", (event:object)=>{
-   
-       
-    
-  
-
-
-
-
-
-
-})
-       
+    sideBar.forEach((bar,index)=>{
+      bar.addEventListener("click", ()=>{
+        renderSideBarNav(index)
+      })
     })
-}
+ }
+   
+
+  function renderSideBarNav(index:number):void{
+    try {
+        console.log("sdfsdfsdf")
+      for(let i=0; i < sideBar.length; i++);
+
+    } catch (error) {
+        
+    }
+  }
 
 
+
+
+
+
+
+
+
+
+// get localstorage.
 function spanWelcome(){
    let newUser = localStorage.getItem("user")!;
         
--- view/dist/dashboardView.js
+++ view/dist/dashboardView.js
@@ -1,14 +1,23 @@
 window.onload = spanWelcome;
 var spanWel = document.querySelector(".container__h2Welcome__spanWelcome");
-var showMonitor = document.querySelector(".showMonitor");
-var showCalc = document.querySelector(".showCalculator");
 var sideBar = document.querySelectorAll("#sideBar");
 if (sideBar) {
-    sideBar.forEach(function (bars) {
-        bars.addEventListener("click", function (event) {
+    sideBar.forEach(function (bar, index) {
+        bar.addEventListener("click", function () {
+            renderSideBarNav(index);
         });
     });
 }
+function renderSideBarNav(index) {
+    try {
+        console.log("sdfsdfsdf");
+        for (var i = 0; i < sideBar.length; i++)
+            ;
+    }
+    catch (error) {
+    }
+}
+// get localstorage.
 function spanWelcome() {
     var newUser = localStorage.getItem("user");
     spanWel.innerHTML = newUser;
