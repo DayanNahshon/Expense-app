2b1334966882ece2e3a9d98e350f4f9b27d87335
Dayan <dayannbryan1@gmail.com>
2023-02-24 12:34:17 +0200

    Update

--- view/dashboardView.ts
+++ view/dashboardView.ts
@@ -1,52 +1,83 @@
+//-------------------Dashboard
+
 window.onload = spanWelcome;
 const spanWel = document.querySelector(
   ".container__h2Welcome__spanWelcome"
 ) as HTMLSpanElement;
-// all sidebar stuff
+// ------------------------------
+
+//-----SideBar
+
+//--SideBar Stuff
 const sideMonitoring = document.querySelector("#sideBarMonitor")as HTMLImageElement;
 const sidecalc = document.querySelector("#sideBarCalc")as HTMLImageElement;
 const sideCurrency = document.querySelector("#sideBarCurrency")as HTMLImageElement;
 const sideMemo = document.querySelector("#sideBarMemo")as HTMLImageElement;
 const sideMoney = document.querySelector("#sideBarMoney")as HTMLImageElement;
+// ------------------------------
 
-// main dashboard
+//--Sidebar Button Events
+sideMonitoring.addEventListener("click", runMonitoring);
+sidecalc.addEventListener("click", runCalculator);
+sideCurrency.addEventListener("click", runCurrency);
+sideMemo.addEventListener("click", runMemo);
+sideMoney.addEventListener("click", runMoney);
+// ------------------------------
 
+//-----Main
+
+//--Main Dashboard Stuff
 const showMonitor = document.querySelector(".showMonitor")as HTMLDivElement;
 const showCalculator = document.querySelector(".showCalculator")as HTMLDivElement;
 const showCurrency = document.querySelector(".showCurrency")as HTMLDivElement;
 const showMemo = document.querySelector(".main__show__showMemo")as HTMLDivElement;
 const showMoney = document.querySelector(".showMoney")as HTMLDivElement;
-    
+// ------------------------------
 
 
+//-----Monitoring
 
-
-
-// sidebar button events
- sideMonitoring.addEventListener("click", runMonitoring);
- sidecalc.addEventListener("click", runCalculator);
- sideCurrency.addEventListener("click", runCurrency);
- sideMemo.addEventListener("click", runMemo);
- sideMoney.addEventListener("click", runMoney);
-
-
-
-
+//--Monitoring Stuff
 
 // ------------------------------
 
-// all localstorage here
+//-----Calc.
 
+//--Calc. Stuff
+const buttonInput = document.querySelectorAll(".calculator__buttons__button") as NodeListOf<HTMLInputElement>
+const inputCalc = document.querySelector("#inputSum") as HTMLInputElement
+const inputTitle = document.querySelector("#inputTitle") as HTMLInputElement
+const buttonClear = document.querySelector("#clear") as HTMLButtonElement
+const buttonAddToTotal = document.querySelector("#addToTotal") as HTMLButtonElement
 
-// --------------------------------
+window.onload = () => {
+  inputCalc.value = ""
+  inputTitle.value = ""
+}
 
-// <!--Dashboard-->
+//--Calc. Events
+buttonInput.forEach((button) => {
+  button.addEventListener("click", () => {
+      //display value of each button
+      console.log("click")
+      inputCalc.value += button.value
+  })
+})
 
-// memo
+//Clear the inputs
+buttonClear.addEventListener("click", () => {
+  inputCalc.value = ""
+  inputTitle.value = ""
+});
+// ------------------------------
+
+//-----Memo
+//--Memo Stuff
 const memoButton = document.querySelector("#memoButton") as HTMLButtonElement;
 let memoInput = document.querySelector("#memoInput") as HTMLInputElement;
 const memoPar = document.querySelector("#memoPar") as HTMLDivElement;
 
+//--Memo Events
 memoButton.addEventListener("click", runMemoTask);
 
 function runMemoTask(e) {
@@ -65,11 +96,13 @@ function runMemoTask(e) {
     return memoInput.required;
   }
 }
-
 // ----------------------------------
 
-// get localstorage.
+
+
+//-----Get LocalStorage.
 function spanWelcome() {
   let newUser = localStorage.getItem("user")!;
      spanWel.innerHTML = newUser;
 }
+
